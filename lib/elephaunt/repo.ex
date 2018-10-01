c607d724070c9961a6202c104f47022e9ca3e92f
santiago vassoler <santiagovassoler@santiagos-mbp.mynet>
2018-10-01 21:20:33 +0100

    initial commit

---
+++ lib/elephaunt/repo.ex
@@ -0,0 +1,11 @@
+defmodule Elephaunt.Repo do
+  use Ecto.Repo, otp_app: :elephaunt
+
+  @doc """
+  Dynamically loads the repository url from the
+  DATABASE_URL environment variable.
+  """
+  def init(_, opts) do
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  end
+end
---
+++ lib/elephaunt_web/router.ex
@@ -0,0 +1,11 @@
+defmodule ElephauntWeb.Router do
+  use ElephauntWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", ElephauntWeb do
+    pipe_through :api
+  end
+end
