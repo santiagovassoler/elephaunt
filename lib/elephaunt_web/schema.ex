33864c8f1e8ddfa47c98e90c27e5e3c82e2421ff
santiago vassoler <santiagovassoler@santiagos-mbp.mynet>
2018-10-02 21:30:06 +0100

    add usertypes to schema

--- lib/elephaunt_web/schema.ex
+++ lib/elephaunt_web/schema.ex
@@ -1,6 +1,10 @@
 defmodule ElephauntWeb.Schema do
   use Absinthe.Schema
 
-  
+  import_types(ElephauntWeb.Schema.Types)
+
+  query do
+      #Fields go here
+  end
 
 end
---
+++ lib/elephaunt_web/schema/types.ex
@@ -0,0 +1,9 @@
+defmodule ElephauntWeb.Schema.Types do
+  use Absinthe.Schema.Notation
+
+  alias ElephauntWeb.Schema.Types
+
+  import_types(Types.Usertype)
+
+  
+end
